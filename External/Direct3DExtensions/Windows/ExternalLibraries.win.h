664fc1339e4c0cb7ebe8b0d8cfd36feba618bf60
u1473700 <LilScorpionC@gmail.com>
2024-08-24 19:43:10 -0600

    Initial Submit

---
+++ External/Direct3DExtensions/Windows/ExternalLibraries.win.h
@@ -0,0 +1,10 @@
+/*
+	This file lists external prebuilt libraries that this library depends on
+*/
+
+#pragma once
+
+// External Libraries
+//===================
+
+#pragma comment( lib, "d3d11.lib" )
---
+++ External/OpenGlExtensions/Windows/ExternalLibraries.win.h
@@ -0,0 +1,12 @@
+/*
+	This file lists external prebuilt libraries that this library depends on
+*/
+
+#pragma once
+
+// External Libraries
+//===================
+
+#pragma comment( lib, "Kernel32.lib" )
+#pragma comment( lib, "Opengl32.lib" )
+#pragma comment( lib, "GlU32.lib" )
